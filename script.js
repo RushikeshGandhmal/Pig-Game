976862d6e8802c26f54b701f9a3a87bcccfcfa21
Rushi <rushikeshgandhmal@gmail.com>
2023-02-26 20:06:02 +0530

    added new files

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+// Comment
+
+// Selecting Elements
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('#score--1'); // an alternative way, faster than querySelctor
+const diceEL = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+
+rollDice.addEventListener('click', function () {
+  const randomNumber = Math.trunc(Math.random() * 6);
+});
