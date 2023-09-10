b038170e53e939eb285fd5a4fd5a6816716bda2a
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-09-10 16:11:42 +0530

    project completed

--- script.js
+++ script.js
@@ -4,22 +4,41 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
-const current0 = document.getElementById('current--0');
-const current1 = document.getElementById('current--1');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const player1 = document.querySelector('.player--0');
 const player2 = document.querySelector('.player--1');
 
-// Initial values
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+const intializeValues = () => {
+  // Initial values
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  btnRoll.classList.remove('hidden');
+  btnHold.classList.remove('hidden');
+
+  document.getElementById(`current--0`).textContent = 0;
+  document.getElementById(`current--1`).textContent = 0;
+
+  player1.classList.remove('player--winner');
+  player2.classList.remove('player--winner');
+
+  document.querySelector(`.player--0`).classList.add('player--active');
+  document.querySelector(`.player--1`).classList.remove('player--active');
+
+  document.querySelector(`.winner--0`).classList.add('hidden');
+  document.querySelector(`.winner--1`).classList.add('hidden');
+};
+
+intializeValues();
 
 function switchPlayer() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -63,13 +82,19 @@ btnHold.addEventListener('click', () => {
     btnRoll.classList.add('hidden');
     btnHold.classList.add('hidden');
     document
+      .querySelector(`.winner--${activePlayer}`)
+      .classList.remove('hidden');
+    document
       .querySelector(`.player--${activePlayer}`)
       .classList.add('player--winner');
     document
       .querySelector(`.player--${activePlayer}`)
       .classList.remove('player--active');
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
   } else {
     // Switch Player
     switchPlayer();
   }
 });
+
+btnNew.addEventListener('click', intializeValues);
